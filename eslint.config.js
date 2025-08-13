10d11832a13ca81fe50d04d37f25188f959e80ff
gpsugy <gpsugy@gmail.com>
2025-08-13 16:58:57 -0700

    Set up eslint + prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,44 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import airbnb from 'eslint-config-airbnb'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
       },
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      prettier: prettier
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+      'import/no-unresolved': 'off',
+      'no-console': 'warn',
+      'prettier/prettier': 'error',
+      ...airbnb.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...prettier.configs.recommended.rules
     },
-  },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
 ])
